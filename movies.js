031b9d0817c49e5613741317649c4b869af4237a
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-17 22:40:47 -0500

    Movies

--- movies.js
+++ movies.js
@@ -2,8 +2,16 @@
 
 
 
-
 const moviesAPI = "https://reliable-accessible-net.glitch.me/movies";
+
+
+fetch('https://reliable-accessible-net.glitch.me/movies')
+    .then((response)=>response.json())
+    .then((jsonData)=> console.log(jsonData))
+
+
+
+
 
 //Get all
 function getMovies(){
@@ -11,6 +19,18 @@ function getMovies(){
 }
 
 
+//Display data
+fetch('https://reliable-accessible-net.glitch.me/movies', {headers: {'Authorization': moviesAPI}})
+    .then((callForJson)=>{
+        return callForJson.json();
+    }).then((movies)=>{
+    console.log(movies[0])
+    $("#movie").append("<div class='card'><h2 class='card-title'>" + "Film" + "</h2>"
+        + "<div class='card-body'><p>" + "Title: " + movies[0].title + "</p>"
+        + "<p>" + "Rating: " + movies[0].rating + "</p>"
+        + "<p>" + "Director: " + movies[0].director + "</p>"
+        + "<p>" + "Plot: " + movies[0].plot + "</p></div>")
+});
 
 
 //Get a by ID
@@ -22,59 +42,59 @@ function getMovie(title){
 
 //function(11)
 
-function editMovie(movie) {
-    let options = {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(movie)
-    }
-    return fetch('${moviesAPI}/${movies.id}' , options)
-        .then((callJson)=>callJson.json())
-}
+// function editMovie(movie) {
+//     let options = {
+//         method: 'PUT',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(movie)
+//     }
+//     return fetch('${moviesAPI}/${movies.id}' , options)
+//         .then((callJson)=>callJson.json())
+// }
+// //
+// let frodo = {
+//     title: 'Frodo',
+//     // isGoodDog: true,
+//     id: 11,
+//     // age: 3
 //
-let frodo = {
-    title: 'Frodo',
-    // isGoodDog: true,
-    id: 11,
-    // age: 3
-
-}
+// }
 
 
-editMovie(frodo).then((data)=>console.log(data))
-
-// Delete by
-function deleteMovie(id){
-    let options = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }
-    fetch('${moviesAPI}/${id}', options)
-        .then((callJson)=>console.log("Delete dog" + id, callJson))
-}
-
-// Create dog
-function addMovie(movie) {
-    let options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(movie)
-    }
-    return fetch('${moviesAPI}/${movies.id}' , options)
-        .then((callJson)=>callJson.json())
-}
-
-let sam = {
-    title: 'Sam',
-    // isGoodDog: true,
-    // age: 3
-
-}
-
-addMovie(sam).then((newDog)=>console.log(newDog))
+// editMovie(frodo).then((data)=>console.log(data))
+//
+// // Delete by
+// function deleteMovie(id){
+//     let options = {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     }
+//     fetch('${moviesAPI}/${id}', options)
+//         .then((callJson)=>console.log("Delete movie" + id, callJson))
+// }
+//
+// // Create Movie
+// function addMovie(movie) {
+//     let options = {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(movie)
+//     }
+//     return fetch('${moviesAPI}/${movies.id}' , options)
+//         .then((callJson)=>callJson.json())
+// }
+//
+// let sam = {
+//     title: 'Sam',
+//     // isGoodDog: true,
+//     // age: 3
+//
+// }
+//
+// addMovie(sam).then((newMovie)=>console.log(newMovie))
